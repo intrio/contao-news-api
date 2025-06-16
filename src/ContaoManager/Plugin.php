c2a682eb62788d068dd6ab716330347cc6cf6c29
Fabian Britsch <info@intermedia.io>
2025-06-16 16:04:47 +0200

    Fix Contao plugin signature (ParserInterface instead of PluginConfig)

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,3 +1,7 @@
+<?php
+
+namespace Intermediaio\ContaoNewsApi\ContaoManager;
+
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
