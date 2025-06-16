0062a086fc98f2395eb304d6a08b1d4bbb38d571
Fabian Britsch <info@intermedia.io>
2025-06-16 15:46:52 +0200

    Fix Contao plugin signature (ParserInterface instead of PluginConfig)

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,19 +1,15 @@
-<?php
-namespace Intermediaio\ContaoNewsApi\ContaoManager;
-
-use Contao\CoreBundle\ContaoCoreBundle;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
-use Contao\ManagerPlugin\Bundle\Config\PluginConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 
 class Plugin implements BundlePluginInterface
 {
-    public function getBundles(PluginConfig $pluginConfig): array
+    public function getBundles(ParserInterface $parser): array
     {
         return [
-          BundleConfig::create(\Intermediaio\ContaoNewsApi\IntermediaioContaoNewsApiBundle::class)
-            ->setLoadAfter([ContaoCoreBundle::class])
+            BundleConfig::create()
+                ->setName('Intermediaio\ContaoNewsApi\IntermediaioContaoNewsApiBundle')
+                ->setLoadAfter(['Contao\CoreBundle']),
         ];
     }
 }
