617518777c7f53b7556ac004b0585eecc1f04746
Fabian Britsch <info@intermedia.io>
2025-06-18 22:19:47 +0200

    Plugin fixed

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,6 +1,13 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{newsapi_legend},newsapi_newsArchive,newsapi_publishByDefault';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{newsapi_legend},newsapi_newsArchive,newsapi_publishByDefault,newsapi_apiKey';
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['newsapi_apiKey'] = [
+    'label'     => ['API‑Key', 'Authentifizierungsschlüssel für externe API-Zugriffe.'],
+    'inputType' => 'text',
+    'eval'      => ['maxlength' => 255, 'tl_class' => 'clr w50'],
+    'sql'       => "varchar(255) NOT NULL default ''"
+];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['newsapi_newsArchive'] = [
   'label'             => ['News-Archiv (API)', 'Archiv, in das Beiträge per API geschrieben werden.'],
