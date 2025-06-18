f9be3046a7409f2a98f80e48ac03f10d24ad7d1e
Fabian Britsch <info@intermedia.io>
2025-06-18 17:57:13 +0200

    Plugin fixed

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,18 +1,27 @@
 <?php
 
+/**
+ * @copyright   Copyright (c) 2025, intermeida.io GmbH
+ * @author      Fabian Britsch <info@intermedia.io>
+ */
+
 namespace Intermediaio\ContaoNewsApi\ContaoManager;
 
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Intermediaio\ContaoNewsApi\IntermediaioContaoNewsApiBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(Intermediaio\ContaoNewsApi\IntermediaioContaoNewsApiBundle::class)
-                ->setLoadAfter([\Contao\CoreBundle\ContaoCoreBundle::class]),
+            BundleConfig::create(IntermediaioContaoNewsApiBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                ]),
         ];
     }
 }
