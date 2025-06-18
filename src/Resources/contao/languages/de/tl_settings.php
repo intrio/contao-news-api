ec8d0a7464f2b75f5d8800599e6218c0da60f012
Fabian Britsch <info@intermedia.io>
2025-06-18 22:00:01 +0200

    Plugin fixed

---
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['newsapi_legend'] = 'News-API Einstellungen';
+$GLOBALS['TL_LANG']['tl_settings']['newsapi_newsArchive'] = ['News-Archiv (API)', 'Archiv, in das Beiträge per API geschrieben werden.'];
+$GLOBALS['TL_LANG']['tl_settings']['newsapi_publishByDefault'] = ['API‑Beiträge sofort veröffentlichen', 'Wenn angehakt, werden Beiträge standardmäßig publiziert.'];
